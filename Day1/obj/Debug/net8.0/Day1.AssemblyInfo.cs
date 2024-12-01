226e6440b4ffc484af8896d3a15df0bea32592a8
Hakan Sarıoğlu <61359973+hakansa00219@users.noreply.github.com>
2024-12-01 20:08:39 +0300

    day1

--- Day1/obj/Debug/net8.0/Day1.AssemblyInfo.cs
+++ Day1/obj/Debug/net8.0/Day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1fb448c3a85f83c9fb9d20b01e0b41271d15061")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
